0daba95ef3c07eb06bcaa428c7bd51dc9765baab
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2020-07-20 16:58:45 +0100

    Update bias_2.sh

--- atacseq/tn5_sequence_bias/bias_2.sh
+++ atacseq/tn5_sequence_bias/bias_2.sh
@@ -9,7 +9,7 @@
 ##cat *pos_Tn5_reverse.bed > pos_Tn5_reverse.bed
 
 chmod +x bedtools
-genome="/lustre/scratch109/sanger/pm12/EPIGENODE-ATAC/Homo_sapiens.GRCh38_15.fa"
+genome="Homo_sapiens.GRCh38_15.fa"
 
 #./bedtools getfasta -s -fi $genome -bed pos_Tn5.bed -fo pos_Tn5.fa
 ##Not necessary with jellyfish
@@ -20,18 +20,15 @@ genome="/lustre/scratch109/sanger/pm12/EPIGENODE-ATAC/Homo_sapiens.GRCh38_15.fa"
 #sed -i 's/^.*chr.*$/>chr/' pos_Tn5_forward.fa
 #sed -i 's/^.*chr.*$/>chr/' pos_Tn5_reverse.fa
 
-DIRDATA="/lustre/scratch109/sanger/pm12/biasEPIGENODE"
+DIRDATA="../biasEPIGENODE"
 
 #cat FASTAs
 
 #Create SeqBias File
-##cd /lustre/scratch109/sanger/pm12/R/R-3.2.1/bin
-##./Rscript /lustre/scratch109/sanger/pm12/biasEPIGENODE/seqTools.R
-./jellyfish count -m 6 -s 3G -t 1 pos_Tn5.fa  # -C
-./jellyfish dump mer_counts.jf > mer_counts_dumps.fa
+jellyfish count -m 6 -s 3G -t 1 pos_Tn5.fa  # -C
+jellyfish dump mer_counts.jf > mer_counts_dumps.fa
 
 #process jellyfish output
-cd /lustre/scratch109/sanger/pm12/R/R-3.2.1/bin
-./Rscript /lustre/scratch109/sanger/pm12/biasEPIGENODE/jellyfish.R
+Rscript jellyfish.R
 
 
