bd3d19aca76434dd40360b74f3754836bac7d6c5
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2020-07-20 17:01:00 +0100

    Update seqTools.R

--- atacseq/tn5_sequence_bias/seqTools.R
+++ atacseq/tn5_sequence_bias/seqTools.R
@@ -1,10 +1,9 @@
-
 library(seqTools)
 
 res <- countFastaKmers(filenames = "pos_Tn5.fa", k=6)
 
 
-#Build "SeqBias.txt" file for ATAC-seq
+# Build "SeqBias.txt" file for ATAC-seq
 x<-read.table("GRCh38_Kmers.txt", head=F)  #relative freq of each 6-mer in the human genome
 
 N <- sum(res[,1]) 
@@ -13,6 +12,4 @@ res[,1] <-  ( res[,1] / N ) / x$V2    # 100 *
 
 
 write.table(x=res, file = "SeqBiasTn5.txt", append = FALSE, quote = F, sep = " ", row.names = T, col.names = F) ;
-
-
 
