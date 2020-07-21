16a33c7516e76a3799a459cad24bf0e65be20f05
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2020-07-21 11:46:28 +0100

    Update diffNGS_example.R

--- atacseq/diffNGS/diffNGS_example.R
+++ atacseq/diffNGS/diffNGS_example.R
@@ -1,24 +1,19 @@
-rm(list=objects())
-graphics.off()
-
-
 # Get regions of open chromatin (atac-seq) changing significantly between condition 1 (C1) and condition 2 (C2) 
+
 args<-commandArgs(TRUE)
 
-
 #set working dir
-#setwd("/Users/pm12/Desktop/EPIGENODE-manuscript/diffNGS")
-setwd("/lustre/scratch117/cellgen/team204/pm12/scratch109/EPIGENODE-ATAC2/diffNGS")
+setwd("../diffNGS")
 
 ### Parameters of the Analysis
-  adjP <- 1e-4  #1e-6  ( Bonferroni adj P-value cutoff )  
+  adjP <- 1e-4   # ( Bonferroni adj P-value cutoff )  
   AbsFc <- 2
   #Fl <- 750
   Nbasis <- 10
   Bins <- 31
-  CNDS  <- c(as.character(args[2]),as.character(args[3]),as.character(args[4]),as.character(args[5]) )  #c(rep("EG1",2), rep("h12",2) )
-  bws   <- c(as.character(args[6]),as.character(args[7]),as.character(args[8]),as.character(args[9]) )  #c( "ATAC1.norm.bw", "ATAC2.norm.bw", "ATAC9.norm.bw", "ATAC10.norm.bw" )   ### Normalized bigwig files
-  peaks <- as.character(args[1])  #"temp.bed"
+  CNDS  <- c(as.character(args[2]),as.character(args[3]),as.character(args[4]),as.character(args[5]) )  
+  bws   <- c(as.character(args[6]),as.character(args[7]),as.character(args[8]),as.character(args[9]) )  
+  peaks <- as.character(args[1]) 
   
  
 ### Load the package or install if not present
@@ -28,16 +23,8 @@ setwd("/lustre/scratch117/cellgen/team204/pm12/scratch109/EPIGENODE-ATAC2/diffNG
   library(GenomicRanges)
   library(RColorBrewer)
 
-#  if (!require("fda"))      { install.packages("fda") } 
-#  if (!require("ICSNP"))    { install.packages("ICSNP") } 
-#  #BioC
-#  source("http://bioconductor.org/biocLite.R")
-#  if (!require("genomation"))     { biocLite("genomation")  } 
-#  if (!require("GenomicRanges"))  { biocLite("GenomicRanges")  } 
-
-
 ### Read bed files with location of aggregated peaks
-  bed <- readGeneric(peaks, keep.all.metadata = FALSE) #TRUE  #[1:REGIONS,]
+  bed <- readGeneric(peaks, keep.all.metadata = FALSE) 
   head(bed)
   length(bed)
 
@@ -46,18 +33,11 @@ results <- data.frame(region.chr=seqnames(bed), region.start=start(bed) ,region.
 head(results)
 
 
-source("diffNGS.R")  # Modified function
+source("diffNGS.R")  
 x <- diffNGS(bedFile= peaks , headerBed=FALSE, bigwigs=bws, conditions=CNDS, pcs = 1, variation = 0.01, nbasis=Nbasis, NB=Bins)
 
 
 source("processDiffNGS.R")
 processDiffNGS(rawPvals = x$p.values, results=results, CNDS = CNDS, AbsFc = AbsFc, adjP = adjP, plotpdf=TRUE, Xmin=-10, Xmax=10, GTF="genes_ensembl_76_transcriptome-GRCh38_15.gtf", W=10e3 )
-
-
-
-
-# end # --------------------------------------------------------------------------------
-
-
 
 
