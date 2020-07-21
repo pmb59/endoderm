e01b414eb8c1b88b286df74fde6f86f4090d67c3
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2020-07-21 13:33:18 +0100

    Update FootprintMixture.sh

--- atacseq/FootprintMixture/FootprintMixture.sh
+++ atacseq/FootprintMixture/FootprintMixture.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-chmod +x bedtools
-
 outDir="../Footprints_plots"
 
-#motifs  in /fimo_occurrences_60k/
+
+#motifs  in /fimo_occurrences_500k/
 cd /../fimo/fimo_occurrences_500k
 
 n=$(ls *txt | sed -e 's/\_..*$//' )
@@ -12,7 +11,7 @@ n=$(ls *txt | sed -e 's/\_..*$//' )
 cd ./FootprintMixture
 
 
-for SAMPLE in ATAC21 ATAC22 ; do   # samples at 36h
+for SAMPLE in ATAC21 ATAC22 ; do   # 2 samples at 36h
 
   for moFile in $(echo $n) ; do    # M1957  M0085 M0820 M0415  ; do
 
