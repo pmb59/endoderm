c996027ba18f80d9e4f5457f1345c9dbd479cb40
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2020-07-21 11:23:11 +0100

    Update run_HOMER.sh

--- atacseq/HOMER/run_HOMER.sh
+++ atacseq/HOMER/run_HOMER.sh
@@ -16,7 +16,7 @@ n=$(wc -l <  ${FASTA})
 
 scrambleFasta.pl $FASTA > ${FASTA}.scramble  -n $(echo $n/2 | bc -l)
 
-# score known motifs for enrichment)
+# score known motifs for enrichment
 homer2 known -p 1 -i $FASTA  -b ${FASTA}.scramble -m $MOTIFS -o ${FASTA}.known
 
 
