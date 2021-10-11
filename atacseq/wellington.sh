73185e0f0f68bf1cafe4c8d450c6371ca67b9a5c
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-10-11 23:52:15 +0100

    Update wellington.sh

--- atacseq/wellington.sh
+++ atacseq/wellington.sh
@@ -2,8 +2,8 @@
 
 # pyDNase-0.2.4
 
-OUTDIR="../wellington";
-BED="../wellington/extended.bed"  # EPIGENODE_merged_ATAC_peaks.bed, extended 50bp each colum (start and end)
+OUTDIR="$(pwd)/wellington";
+BED="$(pwd)/wellington/extended.bed"  # EPIGENODE_merged_ATAC_peaks.bed, extended 50bp each colum (start and end)
 
 #Get Wellington footprints
 
@@ -11,8 +11,6 @@ for i in 1-2 9-10 11-12 13-14 15-16 21-22 17 ; do
 wellington_footprints.py -p 32 -A -fdr 0.1 --FDR_limit -4 --pv_cutoffs -4 -fp 4,30,1 -fdriter 500 --one_dimension -o atac_${i} ${BED} ../mergedBams/ATAC${i}.bam ${OUTDIR}/atac_${i}  ;
 done
 
-
 # Output files can be found at
 # http://ngs.sanger.ac.uk/production/endoderm/
-
 
