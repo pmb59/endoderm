fe94beb3f38f9ca188f7d5e30226994ce7ecb5d5
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-09-18 17:16:34 +0100

    Create csv2bed.R

---
+++ atacseq/intervene/csv2bed.R
@@ -0,0 +1,12 @@
+
+setwd( "./diffNGS.Diff_Open_Chromatin_Regions.FC_2_adjP_1e-04" )
+
+inputs <- list.files( path = ".", pattern = ".csv" )
+
+for ( i in inputs ){
+  readcsv <- read.csv( i, head=TRUE )
+  print( head( readcsv ) )
+  write.table( readcsv[,1:3], file = paste0( i,'.bed' ) , append = FALSE, quote = FALSE, sep = "\t",
+              eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+              col.names = FALSE)
+}
